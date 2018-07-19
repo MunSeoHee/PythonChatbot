72699aac085797cd7ec3894fd68f548d7f53913f
To <38175311+Titeui@users.noreply.github.com>
2018-07-19 17:06:43 +0900

    Update views.py

--- testest/views.py
+++ testest/views.py
@@ -64,7 +64,7 @@ def answer(request):
 
     elif section == '사료3' :
         if datacontent == '임신':
-            food = (int(weight[0]) * 30 + 70) * 1.5 * 3 / 4.5
+            food = ((weight[0]) * 30 + 70) * 1.5 * 3 / 4.5
             return JsonResponse({
                 'message': {
                     'text': "%d g의 사료가 필요합니다! \n 종이컵으로 약 %.1f 컵 정도예요!"%(food,food/78)
@@ -74,7 +74,7 @@ def answer(request):
                 }
             })
         elif datacontent=='비만':
-            food = (int(weight[0]) * 30 + 70) * 1.5 / 4.5
+            food = ((weight[0]) * 30 + 70) * 1.5 / 4.5
             return JsonResponse({
                 'message': {
                     'text': "%d g의 사료가 필요합니다! \n 종이컵으로 약 %.1f 컵 정도예요!"%(food,food/78)
@@ -84,7 +84,7 @@ def answer(request):
                 }
             })
         elif datacontent=='해당없음':
-            food = (int(weight[0]) * 30 + 70) * 1.5 * 3 / 4.5
+            food = ((weight[0]) * 30 + 70) * 1.5 * 3 / 4.5
             return JsonResponse({
                 'message': {
                     'text': "%d g의 사료가 필요합니다! \n 종이컵으로 약 %.1f 컵 정도예요!"%(food,food/78)
